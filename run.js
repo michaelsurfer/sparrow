0775cfe7291be7e6ca28ad96f99747e9c3259e95
michael <personal.michael@gmail.com>
2018-07-12 15:28:49 +0800

    new

---
+++ run.js
@@ -0,0 +1,17 @@
+
+var express = require('express');
+var app = express();
+var cors = require('cors');
+const path = require('path');
+
+app.use(cors());
+app.use( express.static( `build` ) );
+
+
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname, '/build/index.html'));
+});
+
+app.listen(8000,function(){
+	console.log('server running on 8000')
+});
